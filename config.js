1b21871474f57c62b4b62790efeff49ea58f8260
kannkyo <15080890+kannkyo@users.noreply.github.com>
2019-02-05 21:27:54 +0900

    Add subchart for overview

--- config.js
+++ config.js
@@ -9,36 +9,54 @@ var timeFormat = {
   "shortMonths": ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"]
 };
 
+var width = 600;
+
 // Assign the specification to a local variable vlSpec.
 var vlSpec = {
   "$schema": "https://vega.github.io/schema/vega-lite/v3.json",
   "description": "A simple bar chart with ranged data (aka Gantt Chart).",
-  "width": 600,
-  "selection": {
-    "grid": {
-      "type": "interval", "bind": "scales"
-    }
-  },
   "data": {
     "url": "data.csv"
   },
-  "mark": {"type": "bar"},
-  "encoding": {
-    "y" : {"field": "task", "type": "ordinal"},
-    "x" : {"field": "aos", "type": "temporal"},
-    "x2": {"field": "los"},
-    "tooltip": [
-      {"field": "task", "type": "ordinal"},
-      {"field": "gs", "type": "ordinal"},
-      {"field": "sat", "type": "ordinal"},
-      {"field": "aos", "type": "temporal"},
-      {"field": "los", "type": "temporal"}
-    ]
-  },
-  "config": {
-    "timeFormat": "%Y/%m/%d %H:%M:%S.%L",
-    "axisX": {
-      "format": "%Y/%m/%d %H:%M:%S.%L"
+  "vconcat": [{
+    "width": width,
+    "height": 300,
+    "mark": {"type": "bar"},
+    "encoding": {
+      "y" : {"field": "task", "type": "ordinal"},
+      "x" : {"field": "aos", "type": "temporal", "scale": {"domain": {"selection": "brush"}}},
+      "x2": {"field": "los"},
+      "tooltip": [
+        {"field": "task", "type": "ordinal"},
+        {"field": "gs", "type": "ordinal"},
+        {"field": "sat", "type": "ordinal"},
+        {"field": "aos", "type": "temporal"},
+        {"field": "los", "type": "temporal"}
+      ]
+    },
+    "config": {
+      "timeFormat": "%Y/%m/%d %H:%M:%S.%L",
+      "axisX": {
+        "format": "%Y/%m/%d %H:%M:%S.%L"
+      }
     }
-  }
+  },{
+    "width": width,
+    "height": 60,
+    "selection": {
+      "brush": {"type": "interval", "encodings": ["x"]}
+    },
+    "mark": {"type": "bar"},
+    "encoding": {
+      "y" : {"field": "task", "type": "ordinal"},
+      "x" : {"field": "aos", "type": "temporal"},
+      "x2": {"field": "los"}
+    },
+    "config": {
+      "timeFormat": "%Y/%m/%d %H:%M:%S.%L",
+      "axisX": {
+        "format": "%Y/%m/%d %H:%M:%S.%L"
+      }
+    }
+  }]
 };
